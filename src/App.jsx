4e387c9e0f9b02efaa06f9f002b2a18f6dbcec53
Ankush2107 <ankush.kumar210701@gmail.com>
2024-06-14 17:29:19 +0530

    added info

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import InfoBox from "./components/InfoBox/InfoBox.jsx";
 import SearchBox from "./components/SearchBox/SearchBox.jsx";
 const App = () => {
   return (
     <div>
       <SearchBox/>
+      <InfoBox/>
     </div>
   )
 }
